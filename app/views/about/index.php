9090c195b4caf70b7af93332d4b9dc98c7e5774e
zaskha193040134 <193040134.zaskha@mail.unpas.ac.id>
2021-10-01 23:08:08 +0700

    mempelajari tentang ASSETS pada Aplikasi MVC

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,3 +1,14 @@
 
-        <h1>About me</h1>
+        
         <p>halo nama saya <?= $data['nama'];?>, umur saya <?= $data['umur'];?>, saya adalah<?= $data['pekerjaan'];?></p>
+
+        <div class="container">
+<div class="card" style="width: 18rem;">
+<img src="<?= BASEURL; ?>/img/zaskha.jpg" class="card-img-top" alt="ini adalah gambar">
+  <div class="card-body">
+    <h5 class="card-title">About me</h5>
+    <p class="card-text">halo nama saya <?= $data['nama'];?>, umur saya <?= $data['umur'];?>, saya adalah<?= $data['pekerjaan'];?></p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+</div>
--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,2 +1,9 @@
-
-        <h1>Selamat datang di web saya</h1>
+<div class="container">
+<div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">Selamat datamg di web saya</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+</div>
